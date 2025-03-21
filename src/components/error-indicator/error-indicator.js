2d3dfe422c8d7a5075a26eeb8b81ef366877ac1e
Mashina-Nika <mashina.veronika@list.ru>
2025-03-21 22:47:40 +0500

    3rd commit (error-indicator)

---
+++ src/components/error-indicator/error-indicator.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './error-indicator.css';
+
+const ErrorIndicator = () => {
+    return (
+        <div className="error-indicator">
+            <img src={"src/components/error-indicator/death-star.png"} alt="error icon"/>
+            <span className="boom">BOOM!</span>
+            <span>
+         something has gone terribly wrong
+       </span>
+            <span>
+         (but we already sent droids to fix it)
+       </span>
+        </div>
+    );
+};
+
+export default ErrorIndicator;
